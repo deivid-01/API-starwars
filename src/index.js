8bb5276d14642e1e2f4d760dc7b6a66b645b6e51
Deivid <datcx@hotmail.com>
2021-05-16 09:50:25 -0500

    Adding documentation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
+const fileName = './swagger.json';
+const file = require(fileName);
+    
+const swaggerUI = require('swagger-ui-express');
+//const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerDocoment = require('./swagger.json')
 const { mongoose } = require('./database');
+
 require('dotenv').config({path:'variables.env'});
 
-
 const app = express();
-
 app.set('host',process.env.HOST || '0.0.0.0');
 app.set('port',process.env.PORT || 5000);
+/** 
+file.host="localhddddd";
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+    if (err) return console.log(err);
+   // console.log(JSON.stringify(file));
+ ;
+  });
+*/
+console.log(swaggerUI.serve);
 
+app.use('/api/docs',swaggerUI.serve,swaggerUI.setup(getOPtions()));
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -21,4 +37,23 @@ app.use('/api/swapi',require('./routes/swapi.routes'));
 
 app.listen(app.get('port'),app.get('host'),()=>{
     console.log('Server on port',app.get('port'));
+   
 });
+
+function getOPtions()
+{
+ 
+
+
+    //console.log(swaggerDocoment.host);
+    delete swaggerDocoment.host;
+  //  console.log(swaggerDocoment.host);
+    var options=swaggerDocoment;
+   // options.definition={
+     //   servers:["localhost:5000/api/docs2"]
+    //}
+    //options.apis =['/d'];
+    //console.log(options.apis);
+   // const spects = swaggerJsdoc(options);
+    return options;
+}
--- src/routes/planets.routes.js
+++ src/routes/planets.routes.js
@@ -3,8 +3,36 @@ const router = express.Router();
 const planetCtrl = require('../controllers/planets.controller');
 
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Planets:
+ *          type:Object
+ *          required:
+ *              - name
+ *          properties:
+ *              id:
+ *                  type:string
+ *                  description:The auto generated id of the planet
+ *          example:
+ *              id: 12313
+ *              title: Planet 1
+ *              name: Planet1
+ */
 
+/**
+ * @swagger
+ * /planets:
+ *  get:
+ *      summary: List of planets
+ *      responses:
+ *         200: 
+ *          description: The list of the planets
+ *              
+ */
 router.get('/:id',planetCtrl.getOne);
+
 router.get('/',planetCtrl.getAll);
 
 router.post('/',planetCtrl.createOne);

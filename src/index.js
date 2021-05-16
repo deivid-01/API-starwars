d45b802eea6fc94b1ff99d4015d21acb3441e5ec
Deivid <datcx@hotmail.com>
2021-05-16 10:10:37 -0500

    Allowing Cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const fs = require('fs');
 const fileName = './swagger.json';
@@ -14,21 +15,14 @@ require('dotenv').config({path:'variables.env'});
 const app = express();
 app.set('host',process.env.HOST || '0.0.0.0');
 app.set('port',process.env.PORT || 5000);
-/** 
-file.host="localhddddd";
-fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
-    if (err) return console.log(err);
-   // console.log(JSON.stringify(file));
- ;
-  });
-*/
-console.log(swaggerUI.serve);
 
-app.use('/api/docs',swaggerUI.serve,swaggerUI.setup(getOPtions()));
+
+//Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-
-
+app.use(cors())
+//Routes
+app.use('/api/docs',swaggerUI.serve,swaggerUI.setup(getOPtions()));
 app.use('/api/characters',require('./routes/characters.routes'));
 app.use('/api/planets',require('./routes/planets.routes'));
 app.use('/api/starships',require('./routes/starships.routes'));
